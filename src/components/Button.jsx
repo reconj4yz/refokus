e48548a4d940e4510485e2635429bfe0c0f7b067
reconjayz <jaichawla263@gmail.com>
2024-09-17 12:56:18 +0530

    new btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { IoMdReturnRight } from "react-icons/io";
 
 
-function Button({title = "Start a Product"}) {
+function Button({title = "Get Started"}) {
   return (
     <div className='group border link w-44 h-10 px-4 py-2 bg-white rounded-full overflow-hidden flex items-center justify-between gap-1'>
         <div className='group-hover:-translate-y-[15px] flex flex-col gap-2 transition duration-500 linear translate-y-[17px]'>         
